a6ecfaf47dd8044e9cfcb1a2aead405e774553a6
Danila-Rudakov <danila.r.rudakov@gmail.com>
2026-01-11 11:49:43 +0300

    Update butcher_table.py

--- solvers/butcher_table.py
+++ solvers/butcher_table.py
@@ -1,14 +1,11 @@
 import numpy as np
-
 class ButcherTable:
-    
-    def __init__(self, a, b, c):
-     
-        # Преобразуем список a в двумерный массив
-        self.a = np.array(a, dtype=float)
-        # Преобразуем список b в одномерный
-        self.b = np.array(b, dtype=float)
-        # Преобразуем список c в одномерный 
-        self.c = np.array(c, dtype=float)
-        self.stages = len(b)
+    def __init__(self, A, b, c):
 
+        self.A = np.array(A, dtype=float)
+        self.b = np.array(b, dtype=float)
+        self.c = np.array(c, dtype=float)
+    
+    def get_stages(self):
+
+        return len(self.b)
